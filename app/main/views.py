76a9e3150860bd6b828ca5cf7821c0b10b7285e3
Jay-68 <kelvinngari1995@gmail.com>
2019-10-13 15:36:20 +0300

    import the app instance and start debugger

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,30 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import main
+from ..request import get_sources, get_articles
+from ..models import Sources
 
 # app views
 @main.route('/')
 def index():
-  '''
-  view root page that returns the index page and its data
-  '''
-  return render_template('index.html')
+    '''
+    view root page that returns the index page and its data
+    '''
+    entertainment = get_sources('entertainment')
+    technology = get_sources('technology')
+    business = get_sources('business')
+    sports = get_sources('sports')
+    # print(entertainment[0].name)
+    title = 'News Highlights'
+
+    return render_template('index.html', entertainment=entertainment, technology=technology, business=business, sports=sports)
+
+
+@main.route('/sources/<id>')
+def articles(id):
+    '''
+    routing to article pages
+    '''
+    articles = get_articles(id)
+    title = 'Latest articles'
+
+    return render_template('articles.html', title=title, articles=articles)
