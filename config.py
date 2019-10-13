bc3a8f9171a77d4d46f5a7927395dc8aec2ae19d
Jay-68 <kelvinngari1995@gmail.com>
2019-10-13 15:58:05 +0300

    import os and add configuration for base url and api key

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Config:
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+Config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
